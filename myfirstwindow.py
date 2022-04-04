22c01f9f573aae957f7ddcf936d0b281098d6a63
Mike Spaans <99069233@mydavinci.nl>
2022-04-04 11:03:48 +0200

    explosief venster gebruik verbeterd

--- myfirstwindow.py
+++ myfirstwindow.py
@@ -7,14 +7,22 @@ window = tkinter.Tk()
 
 AchtergrondList = ["white","yellow","orange", "red", "purple", "black" ]
 window.title("My First Window")
+Grootte = 50
+# WindowGrootte = str(Grootte)+"x"+str(Grootte)
 window.geometry("50x50")
 Aftellen = 6
 
 for i in range(6):
-    window.after(2000, window.configure(bg=AchtergrondList[i]))
-    window.update()
     print (Aftellen)
+    Grootte += 50
+    WindowGrootte = str(Grootte)+"x"+str(Grootte)
     Aftellen -= 1
+    window.geometry(WindowGrootte)
+    window.configure(bg=AchtergrondList[i])
+    window.after(2000,window.update())
+    # Grootte += 50
+    # WindowGrootte = str(Grootte)+"x"+str(Grootte)
+    
 
 time.sleep(2)
 window.destroy()
